b04c29c01b63b53102409eaaa35469f383452482
Shahar <shaharm22@gmail.com>
2024-06-12 20:29:45 +0300

    ContactMe grid

--- src/pages/contactMe/ContactMe.tsx
+++ src/pages/contactMe/ContactMe.tsx
@@ -1,11 +1,9 @@
-import React, { ChangeEvent, useEffect, useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import './ContactMe.css';
-import TextField from '@mui/material/TextField';
-import FullButton from '../../components/ui/fullButton/FullButton';
+import { Grid } from '@mui/material';
 import CustomTextField from '../../components/ui/CustomTextField';
 import { postData } from '../../components/api/api';
 import TruckButton from '../../components/ui/trackButton/TruckButton';
-import EmptyButton from '../../components/ui/emptyButton/EmptyButton';
 
 const ContactMe = () => {
     const [contactInfo, setContactInfo] = useState({
@@ -16,43 +14,39 @@ const ContactMe = () => {
         date: ''
     });
 
-    const [errorFields, seterrorFields] = useState<{ [key: string]: boolean }>(
-        {
-            name: false,
-            phone: false,
-            email: false,
-            message: false,
-        }
-    )
+    const [errorFields, setErrorFields] = useState<{ [key: string]: boolean }>({
+        name: false,
+        phone: false,
+        email: false,
+        message: false,
+    });
 
     const handleInputChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         const { name, value } = event.target;
-        let erorfieldsSingle = false;
+        let errorField = false;
         switch (name) {
             case 'name':
                 const nameParts = value.trim().split(/\s+/);
-                erorfieldsSingle = !(nameParts.length >= 2 && nameParts.every(part => part.length >= 2));
+                errorField = !(nameParts.length >= 2 && nameParts.every(part => part.length >= 2));
                 break;
             case 'phone':
                 const phoneRegex = /^0\d{9}$/;
-                erorfieldsSingle = !phoneRegex.test(value)
+                errorField = !phoneRegex.test(value);
                 break;
             case 'email':
                 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-                erorfieldsSingle = !emailRegex.test(value)
+                errorField = !emailRegex.test(value);
                 break;
             case 'message':
                 const words = value.trim().split(/\s+/);
-                erorfieldsSingle = !(words.length >= 3);
+                errorField = !(words.length >= 3);
                 break;
         }
 
-
-        seterrorFields((prev) => ({
+        setErrorFields((prev) => ({
             ...prev,
-            [name]: erorfieldsSingle,
+            [name]: errorField,
         }));
-
 
         setContactInfo((prev) => ({
             ...prev,
@@ -60,11 +54,9 @@ const ContactMe = () => {
         }));
     };
 
-
     const submit = async () => {
         try {
-            const date = new Date;
-
+            const date = new Date();
             const updatedContactInfo = {
                 ...contactInfo,
                 date: date.toString(),
@@ -91,63 +83,66 @@ const ContactMe = () => {
             contactInfo.name === '' ||
             contactInfo.phone === ''
         );
-    }
+    };
 
     return (
         <>
             <h2 className='title commenProperties'>Contact Me</h2>
-            <div className='shortfieldssContainer commenProperties'>
-                <CustomTextField
-                    required
-                    id="outlined-required"
-                    label="Full Name"
-                    name="name"
-                    value={contactInfo.name}
-                    onChange={handleInputChange}
-                    error={errorFields['name']}
-                    helperText={errorFields['name'] ? 'Invalid name' : ''}
-
-                />
-                <CustomTextField
-                    required
-                    id="outlined-required"
-                    label="Phone"
-                    name="phone"
-                    value={contactInfo.phone}
-                    onChange={handleInputChange}
-                    error={errorFields['phone']}
-                    helperText={errorFields['phone'] ? 'Invalid phone number' : ''}
-                />
-                <CustomTextField
-                    required
-                    id="outlined-required"
-                    label="Email"
-                    name="email"
-                    value={contactInfo.email}
-                    onChange={handleInputChange}
-                    error={errorFields['email']}
-                    helperText={errorFields['email'] ? 'Invalid email' : ''}
-
-                />
-            </div>
-            <div className='messageTextConatiner commenProperties'>
-                <CustomTextField
-                    required
-                    id="outlined-multiline-static"
-                    label="Message"
-                    name="message"
-                    multiline
-                    rows={4}
-                    value={contactInfo.message}
-                    onChange={handleInputChange}
-                    error={errorFields['message']}
-                    helperText={errorFields['message'] ? 'Message must contain at least 10 words' : ''}
-
-                />
-            </div>
-            <div className='submitButtonContainer commenProperties'>
-                <div><TruckButton name='Submit' onClick={submit} disabled={emptyFieldsCheck() || (Object.values(errorFields).some(field => field))} /></div>
-            </div>
+            <Grid container spacing={3} className='shortFieldsContainer commenProperties'>
+                <Grid item xs={12} sm={6} md={4} lg={4}>
+                    <CustomTextField
+                        required
+                        id="outlined-required"
+                        label="Full Name"
+                        name="name"
+                        value={contactInfo.name}
+                        onChange={handleInputChange}
+                        error={errorFields['name']}
+                        helperText={errorFields['name'] ? 'Invalid name' : ''}
+                    />
+                </Grid>
+                <Grid item xs={12} sm={6} md={4} lg={4}>
+                    <CustomTextField
+                        required
+                        id="outlined-required"
+                        label="Phone"
+                        name="phone"
+                        value={contactInfo.phone}
+                        onChange={handleInputChange}
+                        error={errorFields['phone']}
+                        helperText={errorFields['phone'] ? 'Invalid phone number' : ''}
+                    />
+                </Grid>
+                <Grid item xs={12} sm={6} md={4} lg={4}>
+                    <CustomTextField
+                        required
+                        id="outlined-required"
+                        label="Email"
+                        name="email"
+                        value={contactInfo.email}
+                        onChange={handleInputChange}
+                        error={errorFields['email']}
+                        helperText={errorFields['email'] ? 'Invalid email' : ''}
+                    />
+                </Grid>
+                <Grid item xs={12} lg={12} className='messageTextContainer'>
+                    <CustomTextField
+                        required
+                        id="outlined-multiline-static"
+                        label="Message"
+                        name="message"
+                        multiline
+                        rows={4}
+                        value={contactInfo.message}
+                        onChange={handleInputChange}
+                        error={errorFields['message']}
+                        helperText={errorFields['message'] ? 'Message must contain at least 10 words' : ''}
+                    />
+                </Grid>
+                <Grid item xs={12} className='submitButtonContainer'>
+                    <TruckButton name='Submit' onClick={submit} disabled={emptyFieldsCheck() || (Object.values(errorFields).some(field => field))} />
+                </Grid>
+            </Grid>
         </>
     );
 };

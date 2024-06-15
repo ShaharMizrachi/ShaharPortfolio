5ab6914e4d9f17d1d2ddb1870164da0e8459fb91
Shahar <shaharm22@gmail.com>
2024-06-15 21:07:18 +0300

    routing to login admin page

--- src/pages/homepage/HomePage.tsx
+++ src/pages/homepage/HomePage.tsx
@@ -2,17 +2,24 @@ import React, { useEffect } from 'react'
 import './HomePage.css'
 import UserCounter from '../../utils/counter'
 import MainPageContent from '../mainPageContent/MainPageContent'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from '../loginPage/LogInPage'
+
 
 
 
 export const HomePage = () => {
 
-
     return (
-        <div className='background'>
+        <BrowserRouter>
             <UserCounter />
-            <MainPageContent />
-        </div>
+            <div className='background'>
+                <Routes>
+                    <Route path="/" element={<MainPageContent />} />
+                    <Route path="/admin" element={<LoginPage />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
     )
 }
 

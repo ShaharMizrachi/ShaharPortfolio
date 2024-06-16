b876c08a52edaa8b8dd85ded3a4972a2442aa61d
Shahar <shaharm22@gmail.com>
2024-06-16 14:57:17 +0300

    login restore

--- src/components/ui/glowingButton/GlowingButton.tsx
+++ src/components/ui/glowingButton/GlowingButton.tsx
@@ -2,10 +2,10 @@
 import React from 'react';
 import './GlowingButton.css';
 
-const GlowingButton = () => {
+const GlowingButton = ({ onClickLogin }: { onClickLogin: () => void }) => {
     return (
-        <button className={'button'} >
-            <span className={'text'}>Admin</span>
+        <button className={'button'} onClick={onClickLogin} >
+            <span className={'text'}>LogIn</span>
             <span className={'shimmer'}></span>
         </button>
     );
--- src/pages/homepage/HomePage.tsx
+++ src/pages/homepage/HomePage.tsx
@@ -2,17 +2,25 @@ import React, { useEffect } from 'react'
 import './HomePage.css'
 import UserCounter from '../../utils/counter'
 import MainPageContent from '../mainPageContent/MainPageContent'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from '../loginPage/LogInPage'
+import AdminDashboard from '../adminDashboard/AdminDashboard'
+
 
 
 
 export const HomePage = () => {
 
-
     return (
-        <div className='background'>
+        <BrowserRouter>
             <UserCounter />
-            <MainPageContent />
-        </div>
+            <div className='background'>
+                <Routes>
+                    <Route path="/" element={<MainPageContent />} />
+                    <Route path="/admin" element={<LoginPage />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
     )
 }
 

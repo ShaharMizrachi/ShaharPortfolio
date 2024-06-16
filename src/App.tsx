59dad1f78700f6eeaae3d6a03c61b8a84cf56042
Shahar <shaharm22@gmail.com>
2024-06-16 14:47:06 +0300

    handle user zooming

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import HomePage from './pages/homepage/HomePage';
 
 const App = () => {
+
+  useEffect(() => {
+    const handleWheel = (event: WheelEvent) => {
+      if (event.ctrlKey) {
+        event.preventDefault();
+      }
+    };
+
+    document.addEventListener('wheel', handleWheel, { passive: false });
+
+    return () => {
+      document.removeEventListener('wheel', handleWheel);
+    };
+  }, []);
+
+
   return (
     <div className='appContainer'>
       <HomePage />

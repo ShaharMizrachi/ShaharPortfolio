cdbc2b031b8a891179269dbd6802e6f8cd91ee9a
Shahar <shaharm22@gmail.com>
2024-06-17 19:30:07 +0300

    zoom rejection

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,19 @@ import HomePage from './pages/homepage/HomePage';
 
 const App = () => {
 
-  // useEffect(() => {
-  //   const handleWheel = (event: WheelEvent) => {
-  //     if (event.ctrlKey) {
-  //       event.preventDefault();
-  //     }
-  //   };
+  useEffect(() => {
+    const handleWheel = (event: WheelEvent) => {
+      if (event.ctrlKey) {
+        event.preventDefault();
+      }
+    };
 
-  //   document.addEventListener('wheel', handleWheel, { passive: false });
+    document.addEventListener('wheel', handleWheel, { passive: false });
 
-  //   return () => {
-  //     document.removeEventListener('wheel', handleWheel);
-  //   };
-  // }, []);
+    return () => {
+      document.removeEventListener('wheel', handleWheel);
+    };
+  }, []);
 
 
   return (
